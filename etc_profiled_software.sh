ae7e5a51e55d6d29fb7ccc150df037bb91378fb9
Vince Zhang <vince025@163.com>
2021-05-24 12:04:01 +0800

    1.Add GO PATH 2.Enabled mouse in tmux.conf

--- etc_profiled_software.sh
+++ etc_profiled_software.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-ARMGCC=/opt/armgcc/bin
-ARMEABIGCC=/opt/arm_compiler/gcc-arm-none-eabi-4_7-2013q1/bin
+#ARMGCC=/opt/armgcc/bin
+#ARMEABIGCC=/opt/arm_compiler/gcc-arm-none-eabi-4_7-2013q1/bin
 JAVA_HOME=/opt/jdk
 JRE_HOME=$JAVA_HOME/jre
 CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib
 OPT_BIN=/opt/applications/bin
+GO_PATH=/usr/local/go/bin
 
-PATH=$OPT_BIN:$ARMGCC:$ARMEABIGCC:$JAVA_HOME/bin:$PATH
+PATH=$GO_PATH:$OPT_BIN:$JAVA_HOME/bin:$PATH
 export JAVA_HOME JRE_HOME CLASSPATH PATH
