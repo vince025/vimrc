036b3cc6e7c58a4d8712160e3274c608faada3b3
Vince Zhang <qingsheng.zhang@dorabot.com>
2021-07-13 16:01:02 +0800

    Add cross_compiler to path

--- etc_profiled_software.sh
+++ etc_profiled_software.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#ARMGCC=/opt/armgcc/bin
-#ARMEABIGCC=/opt/arm_compiler/gcc-arm-none-eabi-4_7-2013q1/bin
+CROSS_COMPILER=/opt/cross_compiler/gcc-arm-none-eabi/bin
 JAVA_HOME=/opt/jdk
 JRE_HOME=$JAVA_HOME/jre
 CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib
 OPT_BIN=/opt/applications/bin
 GO_PATH=/usr/local/go/bin
+LD_PATH=/usr/local/lib
 
-PATH=$GO_PATH:$OPT_BIN:$JAVA_HOME/bin:$PATH
-export JAVA_HOME JRE_HOME CLASSPATH PATH
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LD_PATH
+PATH=$GO_PATH:$OPT_BIN:$JAVA_HOME/bin:$CROSS_COMPILER:$PATH
+export JAVA_HOME JRE_HOME CLASSPATH PATH LD_LIBRARY_PATH
